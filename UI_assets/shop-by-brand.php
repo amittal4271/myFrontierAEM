92f7fe70680faae94bea670cfd270bc09d7af3c6
Justin Prahst <>
2018-05-10 08:21:32 -0400

    validation updates for name/email feilds on reg page, initial commit of brands landing page

---
+++ UI_assets/shop-by-brand.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+          <?php include(dirname(__FILE__).'/snippets/header-files-include.php');?>
+     </head>
+
+     <body>
+
+          <?php include(dirname(__FILE__).'/snippets/header.php');?>
+
+          <main class="main-content-container account-section-max-1200">
+               <section id="breadcrumbs">
+                    <div class="container">
+                         <ol class="breadcrumb">
+                              <li><a href="#">Home</a></li>
+                              <li class="active">Shop by Brand</li>
+                         </ol>
+                    </div>
+               </section>
+
+               <section id="shop-by-brand-holder">
+                    <div class="featured-icon-center"></div>
+                    <div class="container">
+                         <h2 class="featured-title">Shop By Brand</h2>
+                         <div class="row top-three-brands clearfix">
+                              <div class="col-xs-4">
+                                   <a class="shop-brand-top-three-link" href="#">
+                                        <img class="top-three-image" src="../frontierwholesales/images/shop-brand-one.jpg"/>
+                                   </a>
+                              </div>
+                              <div class="col-xs-4">
+                                   <a class="shop-brand-top-three-link" href="#">
+                                        <img class="top-three-image" src="../frontierwholesales/images/shop-brand-two.jpg"/>
+                                   </a>
+                              </div>
+                              <div class="col-xs-4">
+                                   <a class="shop-brand-top-three-link" href="#">
+                                        <img class="top-three-image" src="../frontierwholesales/images/shop-brand-three.jpg"/>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="row additional-brands clearfix">
+                              <div class="col-md-12">
+
+                                   <div class="row row-of-four-brands">
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">#</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">24 Karite Gold</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">A</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Accessories</a></li>
+                                                  <li><a href="#">Acorelle</a></li>
+                                                  <li><a href="#">Air Scense</a></li>
+                                                  <li><a href="#">Alacer</a></li>
+                                                  <li><a href="#">Alba Botanica</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-additional-brands-letter">
+                                             <ul class="brands-sub-list">
+                                                  <li><a href="#">Accessories</a></li>
+                                                  <li><a href="#">Acorelle</a></li>
+                                                  <li><a href="#">Air Scense</a></li>
+                                                  <li><a href="#">Alacer</a></li>
+                                                  <li><a href="#">Alba Botanica</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">B</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Babo Botanicals</a></li>
+                                                  <li><a href="#">Bach Flower Remedies</a></li>
+                                                  <li><a href="#">Bad Air Sponge</a></li>
+                                                  <li><a href="#">Bad Again</a></li>
+                                                  <li><a href="#">Bagito</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">C</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Caveman Foods</a></li>
+                                                  <li><a href="#">Celestial Seasonings</a></li>
+                                                  <li><a href="#">Chandrika</a></li>
+                                                  <li><a href="#">ChicoBag</a></li>
+                                                  <li><a href="#">Choice Teas</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                   </div>
+
+                                   <div class="row row-of-four-brands">
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">D</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Dapple</a></li>
+                                                  <li><a href="#">Dear North</a></li>
+                                                  <li><a href="#">Deep Steep</a></li>
+                                                  <li><a href="#">Deodorant Stones of America</a></li>
+                                                  <li><a href="#">Derma E</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">E</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">E-Cloth</a></li>
+                                                  <li><a href="#">Earth Friendly Products</a></li>
+                                                  <li><a href="#">Earth Henna</a></li>
+                                                  <li><a href="#">Earth Kind</a></li>
+                                                  <li><a href="#">Earth Mama Angel Baby</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">F</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Faber Castell</a></li>
+                                                  <li><a href="#">Farmers' Market</a></li>
+                                                  <li><a href="#">Fat Toad Farm</a></li>
+                                                  <li><a href="#">Fatco</a></li>
+                                                  <li><a href="#">Feel Goodz</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6 col-md-3 col-each-letter">
+                                             <h2 class="shop-brand-letter">G</h2>
+                                             <ul class="first-five">
+                                                  <li><a href="#">Genceutic Naturals</a></li>
+                                                  <li><a href="#">GeoDeo</a></li>
+                                                  <li><a href="#">Giovanni</a></li>
+                                                  <li><a href="#">GladRags</a></li>
+                                                  <li><a href="#">Golden Eagle</a></li>
+                                                  <li><a class="shop-by-brand-view-more" href="#">View more brands</a></li>
+                                             </ul>
+                                        </div>
+                                   </div>
+
+                              </div>
+                         </div>
+                    </div>
+               </section>
+          </main>
+
+          <?php include(dirname(__FILE__).'/snippets/footer.php');?>
+
+          <?php include(dirname(__FILE__).'/snippets/footer-files-include.php');?>
+     </body>
+</html>
