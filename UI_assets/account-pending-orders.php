f01fcc1c93ddf63cb26ca0429586e6957794e734
Justin Prahst <>
2018-06-12 10:12:37 -0400

    adding approval section on pending orders in account pages

--- UI_assets/account-pending-orders.php
+++ UI_assets/account-pending-orders.php
@@ -588,10 +588,104 @@
                               </div>
                               
                          </section>
+
+                         <section class="cart-holder cart-actions">
+                              <div class="cart-header-holder no-border">
+                                   <h1 class="cart-heading">Approval Actions</h1>
+                              </div>
+                              <div id="cart-actions-grid">
+                                   <div class="each-action-holder cart-action-status">
+                                        <div class="action-holder">
+                                             <a href="#" class="btn btn-dark-orange btn-checkout-approve-orders" data-toggle="modal" data-target="#modalApproveOrders">Approve Orders</a>
+                                        </div>
+                                   </div>
+                                   <div class="each-action-holder cart-action-status">
+                                        <div class="action-holder">
+                                             <a href="#" class="btn btn-dark-orange btn-checkout-mark-draft" data-toggle="modal" data-target="#modalMarkDraftOrders">Mark as Draft</a>
+                                        </div>
+                                   </div>
+                                   <div class="each-action-holder cart-action-status last">
+                                        <div class="action-holder">
+                                             <a href="#" class="btn btn-red btn-checkout-delete-orders" data-toggle="modal" data-target="#modalDeleteOrders">Delete Orders</a>
+                                        </div>
+                                   </div>
+                              </div>
+
+                              <div class="submit-holder checkout-flow">
+                                   <button class="btn btn-light-green btn-larger btn-cart-checkout">Checkout</button>
+                              </div>
+
+                         </section>
                     </div>
                </section>
 
           </main>
+
+          <!-- Delete Order Modal -->
+          <div class="modal fade" id="modalDeleteOrders" tabindex="-1" role="dialog" aria-labelledby="modalDeleteOrderLabel">
+               <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                         <div class="modal-header clearfix">
+                              <h4 class="modal-title" id="modalDeleteOrderLabel">Delete Confirmation</h4>
+                         </div>
+                         <div class="modal-body clearfix">
+                              <p>Messaging explaining delete action. This order will be removed from the users cart along with this cart.</p>
+                         </div>
+                         <div class="modal-footer clearfix">
+                              <div class="modal-footer-btn-left-holder">
+                                   <button type="button" class="btn btn-red btn-delete-order">Yes, Delete Orders</button>
+                              </div>
+                              <div class="modal-footer-btn-right-holder">
+                                   <button type="button" class="btn btn-link btn-link-close-modal" data-dismiss="modal">Cancel</button>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
+          <!-- Approve Order Modal -->
+          <div class="modal fade" id="modalApproveOrders" tabindex="-1" role="dialog" aria-labelledby="modalApproveOrderLabel">
+               <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                         <div class="modal-header clearfix">
+                              <h4 class="modal-title" id="modalApproveOrderLabel">Approval Confirmation</h4>
+                         </div>
+                         <div class="modal-body clearfix">
+                              <p>You are approving these orders for purchase. This is another explanation for the approval modal.</p>
+                         </div>
+                         <div class="modal-footer clearfix">
+                              <div class="modal-footer-btn-left-holder">
+                                   <button type="button" class="btn btn-dark-orange btn-delete-order">Yes, Approve Orders</button>
+                              </div>
+                              <div class="modal-footer-btn-right-holder">
+                                   <button type="button" class="btn btn-link btn-link-close-modal" data-dismiss="modal">Cancel</button>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
+          <!-- Mark Draft Order Modal -->
+          <div class="modal fade" id="modalMarkDraftOrders" tabindex="-1" role="dialog" aria-labelledby="modalMarkDraftOrdersLabel">
+               <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                         <div class="modal-header clearfix">
+                              <h4 class="modal-title" id="modalMarkDraftOrdersLabel">Mark as Draft</h4>
+                         </div>
+                         <div class="modal-body clearfix">
+                              <p>You are marking these orders as a draft. These will not be included in checkout cost.</p>
+                         </div>
+                         <div class="modal-footer clearfix">
+                              <div class="modal-footer-btn-left-holder">
+                                   <button type="button" class="btn btn-dark-orange btn-delete-order">Mark as Draft</button>
+                              </div>
+                              <div class="modal-footer-btn-right-holder">
+                                   <button type="button" class="btn btn-link btn-link-close-modal" data-dismiss="modal">Cancel</button>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
 
           <?php include(dirname(__FILE__).'/snippets/footer.php');?>
 
