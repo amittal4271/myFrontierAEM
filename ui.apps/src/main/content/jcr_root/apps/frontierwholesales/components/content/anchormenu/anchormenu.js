3c294c9ab0c660a6a081bfd50e7e7fa9e2e57c75
Andrija-PC\Andrija <amiljkovic@ztech.io>
2018-06-18 14:40:19 +0200

    Added anchor-menu component and changes in existing components to work with that addition

---
+++ ui.apps/src/main/content/jcr_root/apps/frontierwholesales/components/content/anchormenu/anchormenu.js
@@ -0,0 +1,19 @@
+
+use( function () {
+
+    var linksList = [];
+    var linkArray = properties.get("anchormenu");    
+    if (linkArray != null){
+        for(var i = 0; i < linkArray.length; i++) {
+            var singleObj = {};
+            var itemObject =  JSON.parse(linkArray[i]);
+            singleObj['title'] = itemObject.anchorTitle;
+            singleObj['path'] = itemObject.anchorId;
+            linksList.push(singleObj);   
+    	};
+    } 
+
+   return {
+        linksList: linksList
+    };
+});
