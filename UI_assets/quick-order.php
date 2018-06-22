ed04ca5c1f9026dc9af4fcc49fc1956eeece268a
M3\messick <mmessick@ztech.io>
2018-06-22 12:01:15 -0400

    FCOB-411 Added onclick add 5 additional rows to Quick Order page.

--- UI_assets/quick-order.php
+++ UI_assets/quick-order.php
@@ -138,20 +138,18 @@
                                    </div>
                               </div>
 
-                              <div class="quick-order-actions">
-                                   <button type="button" class="btn btn-link green-link btn-add-more-line-items">
-                                        <span class="glyphicon glyphicon-plus"></span>Add Another Line
-                                   </button>
-                              </div>
-
-                              <div class="quick-order-actions">
-                                   <button type="button" class="btn btn-larger btn-light-green btn-add-all-line-items-to-cart">
-                                        Add Items to Cart
-                                   </button>
-                              </div>
-
+                              
                          </div>
-                         
+                         <div class="quick-order-actions">
+                              <button type="button" class="btn btn-link green-link btn-add-more-line-items">
+                                   <span class="glyphicon glyphicon-plus"></span>Add Another Line
+                              </button>
+                         </div>
+                         <div class="quick-order-actions">
+                              <button type="button" class="btn btn-larger btn-light-green btn-add-all-line-items-to-cart">
+                                   Add Items to Cart
+                              </button>
+                         </div>                        
                     </div>
                </section>
 
@@ -160,5 +158,120 @@
           <?php include(dirname(__FILE__).'/snippets/footer.php');?>
 
           <?php include(dirname(__FILE__).'/snippets/footer-files-include.php');?>
+          <script>
+               ;(function($) {
+
+                    window.quickOrder = {
+                         quickOrderCountInput: $('#quick-order-count-input'),
+                         groupHolder: $('#quick-order-form-holder'),
+                         countMin: 5,
+                         countMax: 100,
+                         currentCount: 5,
+
+                         addNewRowHtml: function () {
+                              var i;
+                              for (i = 0; i < 5; i++){
+                                   quickOrder.currentCount++;
+                                   quickOrder.quickOrderCountInput.val(quickOrder.currentCount);
+                                   var $newRowHtml = '';
+
+                                   $newRowHtml += '<div id="quick-order-section-'+quickOrder.currentCount+'" class="each-quick-order-section">';
+                                        $newRowHtml += '<div class="form-group item-group">';
+                                             $newRowHtml += '<label for="item_code_'+quickOrder.currentCount+'">Item Code</label>';
+                                             $newRowHtml += '<input id="item_code_'+quickOrder.currentCount+' name="item_code_'+quickOrder.currentCount+'" placeholder="Code" type="text" class="form-control">';
+                                        $newRowHtml += '</div>';     
+                                        $newRowHtml += '<div class="form-group qty-group">';
+                                             $newRowHtml += '<label for="item_qty_'+quickOrder.currentCount+'">Qty</label>';
+                                             $newRowHtml += '<input id="item_qty_'+quickOrder.currentCount+'" name="item_qty_'+quickOrder.currentCount+'" placeholder="Qty" type="text" class="form-control">';
+                                        $newRowHtml += '</div>';
+                                        $newRowHtml += '<div class="form-group price-group">';
+                                             $newRowHtml += '<label for="total_price_'+quickOrder.currentCount+'">Total Price</label>';
+                                             $newRowHtml += '<input id="total_price_'+quickOrder.currentCount+'" name="total_price_'+quickOrder.currentCount+'" type="text" class="form-control">';
+                                        $newRowHtml += '</div>';
+                                        $newRowHtml += '<div class="form-group price-group">';
+                                             $newRowHtml += '<label for="remove_'+quickOrder.currentCount+'">Remove</label>';
+                                             $newRowHtml += '<button type="button" class="btn btn-only-green-icon btn-remove-quick-order">';
+                                                  $newRowHtml += '<span class="address-link-text visuallyhidden">Remove</span>';
+                                                  $newRowHtml += '<span class="glyphicon glyphicon-remove"></span>';
+                                             $newRowHtml += '</button>';     
+                                        $newRowHtml += '</div>';
+                                   $newRowHtml += '</div>';
+                                   
+                                   quickOrder.groupHolder.append($newRowHtml);
+                              }     
+                         },
+
+                         removeRow: function ($btnEl) {
+                              // find parent invite group holder
+                              var $quickOrderSectionRemoveHolder = $btnEl.parents('.each-quick-order-section');
+                              // remove html
+                              $quickOrderSectionRemoveHolder.remove();
+                              quickOrder.updateLineItemCountsOnRemove();
+                         },
+
+                         updateLineItemCountsOnRemove: function () {
+                              // we need to update numbering based on line item number that is removed
+                              if (quickOrder.currentCount > 4) {
+                                   $('.each-quick-order-section').each(function (index, value) {
+                                        var $this = $(this);
+                                        var $nameGroup = $this.children('.name-group');
+                                        var $emailGroup = $this.children('.email-group');
+                                        console.log(index+1);
+                                        // set index to match numbers starting at 1
+                                        var $newIndex = index+1;
+                                        // reset html that has numbers according to new index
+                                        $this.attr('id', 'quick-order-section-'+$newIndex);
+
+                                        // set new index values on labels and inputs for name and email
+                                        var $nameLabel = $nameGroup.find('label');
+                                        var $nameInput = $nameGroup.find('input');
+
+                                        $nameLabel.attr('for', 'id_invite-'+$newIndex+'-name');
+                                        $nameInput.attr('id', 'id_invite-'+$newIndex+'-name');
+                                        $nameInput.attr('name', 'invite_'+$newIndex+'_name');
+
+                                        var $emailLabel = $emailGroup.find('label');
+                                        var $emailInput = $emailGroup.find('input');
+
+                                        $emailLabel.attr('for', 'id_invite-'+$newIndex+'-email');
+                                        $emailInput.attr('id', 'id_invite-'+$newIndex+'-email');
+                                        $emailInput.attr('name', 'invite_'+$newIndex+'_email');
+                                   });
+                              }
+                         }
+                    };
+
+               })(jQuery);
+
+               $(document).ready(function() {
+
+
+                    $(document).on( "click", ".btn-add-more-line-items", function(e) {
+                         var $this = $(this);
+                         // update current count 
+                         
+                         // log
+                         console.log(quickOrder.currentCount);
+                         // update input count val
+                         
+
+                         quickOrder.addNewRowHtml();
+                    });
+
+                    $(document).on( "click", ".btn-remove-quick-order", function(e) {
+                         var $this = $(this);
+                         var $btnEl = $this;
+                         // update current count 
+                         quickOrder.currentCount--;
+                         // log
+                         console.log(quickOrder.currentCount);
+                         // update input count val
+                         quickOrder.quickOrderCountInput.val(quickOrder.currentCount);
+                         // remove row
+                         quickOrder.removeRow($btnEl);
+                    });
+                    
+               });
+          </script>
      </body>
 </html>
