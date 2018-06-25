a4495c821601ea622348649daa8addc45e0ad4ee
Andrija-PC\Andrija <amiljkovic@ztech.io>
2018-06-25 17:19:57 +0200

    Generic image component added to project

--- ui.apps/src/main/content/jcr_root/apps/frontierwholesales/components/content/image/image.js
+++ ui.apps/src/main/content/jcr_root/apps/frontierwholesales/components/content/image/image.js
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2016 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 "use strict";
 
 /**
@@ -23,15 +39,19 @@ use(["/libs/wcm/foundation/components/utils/AuthoringUtils.js",
     }, function() {
         // The drag & drop image CSS class name
         image.cssClass = CONST.AUTHOR_IMAGE_CLASS;
-        
+
         // Modifying the image object to set the placeholder if the content is missing
+
         if (!image.fileReference()) {
-            
-            image.src = CONST.PLACEHOLDER_SRC;
-            if (AuthoringUtils.isTouch) {
-                image.cssClass = image.cssClass + " " + CONST.PLACEHOLDER_TOUCH_CLASS;
-            } else if (AuthoringUtils.isClassic) {
-                image.cssClass = image.cssClass + " " + CONST.PLACEHOLDER_CLASSIC_CLASS;
+            if(typeof wcmmode != "undefined" && wcmmode.isEdit()) {
+                image.src = CONST.PLACEHOLDER_SRC;
+                if (AuthoringUtils.isTouch) {
+                    image.cssClass = image.cssClass + " " + CONST.PLACEHOLDER_TOUCH_CLASS;
+                } else if (AuthoringUtils.isClassic) {
+                    image.cssClass = image.cssClass + " " + CONST.PLACEHOLDER_CLASSIC_CLASS;
+                }
+            } else {
+                image.src = "";
             }
         }
         
@@ -40,6 +60,15 @@ use(["/libs/wcm/foundation/components/utils/AuthoringUtils.js",
     
     // Adding the constants to the exposed API
     image.CONST = CONST;
+    
+    // check for image available sizes
+    if (image.width() <= 0) {
+        image.width = "";
+    }
+    
+    if (image.height() <= 0) {
+        image.height = "";
+    }
     
     return imageDefer.promise;
     
