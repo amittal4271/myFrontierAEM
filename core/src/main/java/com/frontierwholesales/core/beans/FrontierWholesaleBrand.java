10d2ee7e78821ebaf7a84c1a9676d665d05e180d
Adam Yocum <adamyocum@Adams-MacBook-Pro.local>
2018-07-20 01:03:22 -0600

    FCOB-424 shop by brand list page integration with magento endpoint

---
+++ core/src/main/java/com/frontierwholesales/core/beans/FrontierWholesaleBrand.java
@@ -0,0 +1,30 @@
+package com.frontierwholesales.core.beans;
+
+import com.google.gson.JsonObject;
+
+public class FrontierWholesaleBrand {
+
+	private String name;
+	private String id;
+	
+	public FrontierWholesaleBrand() {
+		
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
