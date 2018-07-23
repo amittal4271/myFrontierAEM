2177092c401312f8d57f22b0a0a00b34d4aab109
Adam Yocum <adamyocum@Adams-MacBook-Pro.local>
2018-07-23 01:56:57 -0600

    initial search filter facet component impl, needs continued work to integrate with search results

---
+++ core/src/main/java/com/frontierwholesales/core/magento/services/models/SearchFacets.java
@@ -0,0 +1,69 @@
+package com.frontierwholesales.core.magento.services.models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.frontierwholesales.core.beans.FrontierWholesaleBrand;
+import com.frontierwholesales.core.magento.models.MagentoFacet;
+import com.frontierwholesales.core.magento.services.FrontierWholesalesMagentoCommerceConnector;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@Model(adaptables = { SlingHttpServletRequest.class, Resource.class })
+public class SearchFacets extends BaseModel {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SearchFacets.class);
+
+	private String searchFacetsListResponse;
+
+	private FrontierWholesalesMagentoCommerceConnector connector = new FrontierWholesalesMagentoCommerceConnector();
+
+	private ObjectMapper mapper = new ObjectMapper();
+	private JsonParser parser = new JsonParser();
+	
+	private ArrayList<MagentoFacet> facetList = new ArrayList<MagentoFacet>();
+
+
+	@PostConstruct
+	protected void init() {
+		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+		try {
+			searchFacetsListResponse = connector.getProductFacets(connector.getAdminToken());
+			
+			Gson json = new Gson();
+			JsonElement element = json.fromJson(searchFacetsListResponse, JsonElement.class);
+			//JsonElement catElement = json.fromJson(categories, JsonElement.class);
+			
+			JsonObject object = element.getAsJsonObject();
+			JsonArray itemArray = object.getAsJsonArray("items");
+			
+			facetList = mapper.readValue(itemArray.toString(), new TypeReference<List<MagentoFacet>>() {});
+			
+		} catch (Exception e) {
+			LOGGER.error("Error in searchFacets ", e, e.getMessage());
+		}
+	}
+
+	public ArrayList<MagentoFacet> getFacets() {
+		return facetList;
+	}
+
+
+}
