8e1e50d0b70dc673475420aaed60dc0a5441a0f6
Adam Yocum <adamyocum@Adams-MacBook-Pro.local>
2018-08-14 14:09:14 -0600

    FCOB-77 approval workflow

--- core/src/main/java/com/frontierwholesales/core/workflow/CustomStepForApproval.java
+++ core/src/main/java/com/frontierwholesales/core/workflow/CustomStepForApproval.java
@@ -1,22 +1,26 @@
 package com.frontierwholesales.core.workflow;
 
-
+import java.util.Dictionary;
 import java.util.List;
 
 import javax.jcr.RepositoryException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.framework.Constants;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.granite.workflow.WorkflowException;
 import com.adobe.granite.workflow.WorkflowSession;
@@ -28,87 +32,103 @@ import com.adobe.granite.workflow.metadata.MetaDataMap;
 import com.day.cq.mailer.MessageGateway;
 import com.day.cq.mailer.MessageGatewayService;
 
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.SimpleEmail;
-
 //This is a component so it can provide or consume services
 @Component
 @Service
-@Properties({
-  @Property(name = Constants.SERVICE_DESCRIPTION, value = "Frontier Publication Request Email Step."),
-  @Property(name = Constants.SERVICE_VENDOR, value = "Adobe"),
-  @Property(name = "process.label", value = "Frontier Publication Request Email Step.") })
+@Properties({ @Property(name = Constants.SERVICE_DESCRIPTION, value = "Frontier Publication Request Email Step."),
+		@Property(name = Constants.SERVICE_VENDOR, value = "Adobe"),
+		@Property(name = "process.label", value = "Frontier Publication Request Email Step.") })
 public class CustomStepForApproval implements WorkflowProcess {
 
-  protected final Logger log = LoggerFactory.getLogger(this.getClass());
+	protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
-  @Reference
- private MessageGatewayService messageGatewayService;
+	
+	private final static String DEFAULT_EMAIL_FROM = "aem-admin@frontiercoop.com"; 
+	
+	@Property(label = "Email From", description = "Email to be used for 'from' value on approval workflow emails.", value = DEFAULT_EMAIL_FROM)
+	private static final String PROP_EMAIL_FROM = "frontier.workflowEmailFrom"; 
+	
+	@Reference
+	private MessageGatewayService messageGatewayService;
+	
+	private String emailFrom = DEFAULT_EMAIL_FROM;
 
-  public void execute(WorkItem workitem, WorkflowSession wfsession,
-   MetaDataMap metaDataMap) throws WorkflowException {
-  
-  ResourceResolver resolver = wfsession.adaptTo(ResourceResolver.class);
-  UserManager userManager = resolver.adaptTo(UserManager.class);
-  
-  
-  Workflow workflow = workitem.getWorkflow();
-  String payload = (String) workitem.getWorkflowData().getPayload();
-  String initiator = workitem.getWorkflow().getInitiator();
-  
-  Authorizable authorizable = null;
-  String userEmail = null;
-  try {
-   authorizable = userManager.getAuthorizable(initiator);
-  } catch (RepositoryException e1) {
-   // TODO Auto-generated catch block
-   e1.printStackTrace();
-  }
-        try {
-    userEmail = PropertiesUtil.toString(authorizable.getProperty("profile/email"), "");
-  } catch (RepositoryException e1) {
-   // TODO Auto-generated catch block
-   e1.printStackTrace();
-  }
+	public void execute(WorkItem workitem, WorkflowSession wfsession, MetaDataMap metaDataMap)
+			throws WorkflowException {
 
-   String temp = null;
-  List<HistoryItem> list = wfsession.getHistory(workflow);
-  
-   for(int index = list.size()-1; index >=1; index--){
-    HistoryItem previous = list.get(index);
-    temp = (String) previous.getWorkItem().getMetaDataMap().get("name");
-   }
-   
-  try {
+		ResourceResolver resolver = wfsession.adaptTo(ResourceResolver.class);
+		UserManager userManager = resolver.adaptTo(UserManager.class);
 
-    MessageGateway<Email> messageGateway;
+		Workflow workflow = workitem.getWorkflow();
+		String payload = (String) workitem.getWorkflowData().getPayload();
+		String initiator = workitem.getWorkflow().getInitiator();
 
-    Email email = new SimpleEmail();
+		Authorizable authorizable = null;
+		String userEmail = null;
+		try {
+			authorizable = userManager.getAuthorizable(initiator);
+		} catch (RepositoryException e) {
+			log.error("Issue getting user for workflow", e);
+		}
+		try {
+			userEmail = PropertiesUtil.toString(authorizable.getProperty("profile/email"), "");
+		} catch (RepositoryException e) {
+			log.error("Issue getting user email address for workflow", e);
+		}
 
-    String emailToRecipients = userEmail;
-   //String emailCcRecipients = "abc@gmail.com";
+		String workflowApproveRejectSelection = null;
+		List<HistoryItem> list = wfsession.getHistory(workflow);
 
-    email.addTo(emailToRecipients);
-   //email.addCc(emailCcRecipients);
-   email.setSubject("Frontier Wholesale Page Publication Request");
-   email.setFrom("adamyocum@gmail.com");
-   
-   if(temp .equals("approve")){
-    email.setMsg("This message is to inform you that the Frontier Wholesale content has been approved and activated which path is = "+ payload);
-   }else{
-    email.setMsg("This message is to inform you that the Frontier Wholesale content has been Rejected Please modify it which path is = "+ payload);
-   }
+		for (int index = list.size() - 1; index >= 1; index--) {
+			HistoryItem previous = list.get(index);
+			workflowApproveRejectSelection = (String) previous.getWorkItem().getMetaDataMap().get("name");
+		}
 
-    // Inject a MessageGateway Service and send the message
-   messageGateway = messageGatewayService.getGateway(Email.class);
+		try {
 
-    // Check the logs to see that messageGateway is not null
-   messageGateway.send((Email) email);
-  }
+			MessageGateway<Email> messageGateway;
 
-   catch (Exception e) {
-   e.printStackTrace();
-  }
- }
+			Email email = new SimpleEmail();
+
+			String emailToRecipients = userEmail;
+			// String emailCcRecipients = "abc@gmail.com";
+
+			email.addTo(emailToRecipients);
+			// email.addCc(emailCcRecipients);
+			
+			email.setSubject("Frontier Wholesale Page Publication Request");
+			
+			email.setFrom(emailFrom);
+
+			if (workflowApproveRejectSelection.equals("approve")) {
+				email.setMsg(
+						"This message is to inform you that the Frontier Wholesale content has been approved and activated which path is = "
+								+ payload);
+			} else {
+				email.setMsg(
+						"This message is to inform you that the Frontier Wholesale content has been Rejected Please modify it which path is = "
+								+ payload);
+			}
+
+			// Inject a MessageGateway Service and send the message
+			messageGateway = messageGatewayService.getGateway(Email.class);
+
+			// Check the logs to see that messageGateway is not null
+			messageGateway.send((Email) email);
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Activate
+	protected void activate(final ComponentContext context) {
+		log.debug("Activating Frontier Workflow Custom Approval Email Step");
+		
+		Dictionary<?,?> properties = context.getProperties();
+		emailFrom = PropertiesUtil.toString(properties.get(PROP_EMAIL_FROM), DEFAULT_EMAIL_FROM);
+	}
+
 
 }
--- core/src/main/java/com/frontierwholesales/core/workflow/CustomStepForDynamicParticipant.java
+++ core/src/main/java/com/frontierwholesales/core/workflow/CustomStepForDynamicParticipant.java
@@ -16,30 +16,27 @@ import org.apache.felix.scr.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(immediate=true)
+@Component(immediate = true)
 @Service
-@Properties({@org.apache.felix.scr.annotations.Property(name="service.description", value={"Sample Implementation of dynamic participant chooser."}),
-   @org.apache.felix.scr.annotations.Property(name="chooser.label", value={"Workflow Participant Chooser"})
-   })
-public class CustomStepForDynamicParticipant
-  implements ParticipantStepChooser
-{
-  private static final Logger logger = LoggerFactory.getLogger(CustomStepForDynamicParticipant.class);
-   
-  public String getParticipant(WorkItem workitem, WorkflowSession wfSession, MetaDataMap metaDataMap)
-    throws WorkflowException
-  {
-    logger.info("################ Inside the SampleProcessStepChooserImpl GetParticipant ##########################");
-    String participant = "admin";
-    Workflow workflow = workitem.getWorkflow();
-    String initiator = workflow.getInitiator();
-    List<HistoryItem> wfHistory = wfSession.getHistory(workflow);
-    if (!wfHistory.isEmpty()) {
-      participant = initiator;
-    } else {
-      participant = "admin";
-    }
-    logger.info("####### Participant : " + participant + " ##############");
-    return participant;
-  }
+@Properties({
+		@org.apache.felix.scr.annotations.Property(name = "service.description", value = {
+				"Sample Implementation of dynamic participant chooser." }),
+		@org.apache.felix.scr.annotations.Property(name = "chooser.label", value = {
+				"Frontier Workflow Participant Chooser" }) })
+public class CustomStepForDynamicParticipant implements ParticipantStepChooser {
+	private static final Logger logger = LoggerFactory.getLogger(CustomStepForDynamicParticipant.class);
+
+	public String getParticipant(WorkItem workitem, WorkflowSession wfSession, MetaDataMap metaDataMap)
+			throws WorkflowException {
+		String participant = "admin";
+		Workflow workflow = workitem.getWorkflow();
+		String initiator = workflow.getInitiator();
+		List<HistoryItem> wfHistory = wfSession.getHistory(workflow);
+		if (!wfHistory.isEmpty()) {
+			participant = initiator;
+		} else {
+			participant = "admin";
+		}
+		return participant;
+	}
 }
