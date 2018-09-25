0110ca470ec165ccb512339bf868626d18294e0e
Barani Ramakrishnan <bramakrishnan@ztech.io>
2018-09-25 13:09:32 -0400

    FCOB 566
    Disabiling mini cart if we go to cart page

--- ui.apps/src/main/content/jcr_root/apps/frontierwholesales/components/content/shoppingcart/clientlibs/cart.js
+++ ui.apps/src/main/content/jcr_root/apps/frontierwholesales/components/content/shoppingcart/clientlibs/cart.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     
    if($('#magento-cart-iframe').length > 0){
+       $('#minicarttemplate').css('display','none');
         var userToken = getUserToken();
         if(userToken.startsWith('Bearer')  && userToken.trim().length > 0){
             userToken = userToken.replace('Bearer ','');
              $('#magento-cart-iframe').prop('src',serverURL+'/checkout/cart?token='+userToken);
         }else{
+            $('#minicarttemplate').css('display','block');
             location.href="/content/frontierwholesales/en/login.html";
         }
     }
