d3866e11711e56c01b198b859cfdf7878542588a
Anant Mittal <anant.mittal@epsilon.com>
2018-11-06 09:24:47 -0800

    Removed UI-assets folder. A separate repo has been created for it

--- UI_assets/snippets/footer-files-include.php
+++
@@ -1,5 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php dirname(__FILE__) ?>/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="<?php dirname(__FILE__) ?>/prod-assets/frontier-minified.js?time=<?php echo time();?>"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="<?php dirname(__FILE__) ?>/js/ie10-viewport-bug-workaround.js"></script>
